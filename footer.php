4c930dda7dac593fe970855b686309a0b7695138
Ryan Stonebraker <ryanastonebraker@gmail.com>
2019-03-25 13:33:31 -0800

    Adds social media to bottom and fixes logo

--- footer.php
+++ footer.php
@@ -1,6 +1,38 @@
 </main>
 <footer>
-  &copy; 2018 Project Fairness, Inc.
+  <p>&copy; 2018 Project Fairness, Inc.</p>
+  <?php if (is_home()) : ?>
+    <ul class="social-media">
+      <?php if (get_theme_mod('pf_social_media_section_facebook')) : ?>
+        <li>
+          <a href="<?php echo get_theme_mod('pf_social_media_section_facebook'); ?>" target="_blank">
+            <div class="icons8-facebook"></div>
+          </a>
+        </li>
+      <?php endif; ?>
+      <?php if (get_theme_mod('pf_social_media_section_instagram')) : ?>
+        <li>
+          <a href="<?php echo get_theme_mod('pf_social_media_section_instagram'); ?>" target="_blank">
+            <div class="icons8-instagram"></div>
+          </a>
+        </li>
+      <?php endif; ?>
+      <?php if (get_theme_mod('pf_social_media_section_linkedin')) : ?>
+        <li>
+          <a href="<?php echo get_theme_mod('pf_social_media_section_linkedin'); ?>" target="_blank">
+            <div class="icons8-linkedin"></div>
+          </a>
+        </li>
+      <?php endif; ?>
+      <?php if (get_theme_mod('pf_social_media_section_twitter')) : ?>
+        <li>
+          <a href="<?php echo get_theme_mod('pf_social_media_section_twitter'); ?>" target="_blank">
+            <div class="icons8-twitter"></div>
+          </a>
+        </li>
+      <?php endif; ?>
+    </ul>
+  <?php endif; ?>
 </footer>
 <?php wp_footer(); ?>
 </body>
--- header.php
+++ header.php
@@ -21,6 +21,7 @@
     <body>
       <header>
         <div class="nav-spacer"></div>
+
         <nav class="mobile-hide">
           <i class="fas fa-bars"></i>
           <?php
@@ -34,38 +35,6 @@
             <?php endif; ?>
 
         </nav>
-        <?php if (is_home()) : ?>
-          <ul class="social-media">
-            <?php if (get_theme_mod('pf_social_media_section_facebook')) : ?>
-              <li>
-                <a href="<?php echo get_theme_mod('pf_social_media_section_facebook'); ?>" target="_blank">
-                  <div class="icons8-facebook"></div>
-                </a>
-              </li>
-            <?php endif; ?>
-            <?php if (get_theme_mod('pf_social_media_section_instagram')) : ?>
-              <li>
-                <a href="<?php echo get_theme_mod('pf_social_media_section_instagram'); ?>" target="_blank">
-                  <div class="icons8-instagram"></div>
-                </a>
-              </li>
-            <?php endif; ?>
-            <?php if (get_theme_mod('pf_social_media_section_linkedin')) : ?>
-              <li>
-                <a href="<?php echo get_theme_mod('pf_social_media_section_linkedin'); ?>" target="_blank">
-                  <div class="icons8-linkedin"></div>
-                </a>
-              </li>
-            <?php endif; ?>
-            <?php if (get_theme_mod('pf_social_media_section_twitter')) : ?>
-              <li>
-                <a href="<?php echo get_theme_mod('pf_social_media_section_twitter'); ?>" target="_blank">
-                  <div class="icons8-twitter"></div>
-                </a>
-              </li>
-            <?php endif; ?>
-          </ul>
-        <?php endif; ?>
         <section class="logo-container"><section class="logo"></section></section>
         <?php if (!is_home()) {
           if (have_posts()) {
